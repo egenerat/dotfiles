b5debd96e99613d65a3237951974ff2e85201a10
egenerat <emile.generat@outlook.com>
2019-10-20 11:11:20 +0100

    Update cleaning list for lubuntu 18.04

--- lubuntu/clean-lubuntu-18.04.sh
+++ lubuntu/clean-lubuntu-18.04.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt purge fcitx* xpad mtpaint audacious* guvcview xfburn pidgin* sylpheed* transmission* usb-creator-* synaptic lubuntu-software-center simple-scan mplayer gnome-mplayer gdebi* hardinfo \
+sudo apt purge fcitx* xpad mtpaint audacious* guvcview xfburn pidgin* sylpheed* transmission* usb-creator-* synaptic simple-scan mplayer gnome-mplayer gdebi* hardinfo \
 
 # If bluetooth is not needed
 blue* \
@@ -7,3 +7,5 @@ blue* \
 # For a VM
 xfce4-power-manager* \
 
+# For minimal install
+gnumeric* abiword* gnome-mpv gnome-software*
